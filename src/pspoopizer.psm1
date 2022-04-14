d31f12b8b65b5a3e6447a484f4088ae3d2588978
ShortArrow <bamboogeneral@live.jp>
2022-04-14 15:09:01 +0900

    add child func

--- src/pspoopizer.psm1
+++ src/pspoopizer.psm1
@@ -4,7 +4,28 @@ Output fizzbuzz with unko
 .EXAMPLE
 Get-UnkoFizzBuzz -Begin 30 -End 1
 #>
-function Get-PoopFromString {
+function Get-PoopFromChar {
+    
+    Write-Host("契約書だよ。そこに名前を書きな。")
+    
+    $keiyakusho=Read-Host
+    $name=$keiyakusho
+
+    Write-Host("フン。"+$name +"というのかい。贅沢な名だねぇ。")
+
+    $newNameIndex = Get-Random(($name).Length)
+    $newName = $name.Substring($newNameIndex, 1)
+
+    Write-Host("今からお前の名前は"+$newName+"だ。いいかい、"+$newName+"だよ。分かったら返事をするんだ、"+$newName+"!!")
+}
+
+<#
+.SYNOPSIS
+Output fizzbuzz with unko
+.EXAMPLE
+Get-UnkoFizzBuzz -Begin 30 -End 1
+#>
+function Get-PoopsFromString {
     
     Write-Host("契約書だよ。そこに名前を書きな。")
     
