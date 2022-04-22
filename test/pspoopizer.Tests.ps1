6fa709033b23ad1923829b25dc9428fd128c7717
ShortArrow <bamboogeneral@live.jp>
2022-04-22 16:05:57 +0900

    change scope on import-module

--- test/pspoopizer.Tests.ps1
+++ test/pspoopizer.Tests.ps1
@@ -2,7 +2,7 @@ using namespace System.Collections.Generic
 $here = "$(Split-Path -Parent (Split-Path -Parent $PSCommandPath))"
 $sut = (Split-Path -Leaf $PSCommandPath) -replace ".Tests.", "."
 $sut = $sut -replace ".ps1", ".psm1"
-Import-Module $here/src/$sut -Force
+Import-Module $here/src/$sut -Force -Scope Local
 Describe "main" {
     BeforeAll {
     }
